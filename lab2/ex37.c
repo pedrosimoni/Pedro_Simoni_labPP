6cd6bd5f399a8558db20f9c0013adb13cb019980
Pedro Simoni <100897212+pedrosimoni@users.noreply.github.com>
2022-10-20 02:16:52 +0000

    Update ex37.c

--- lab2/ex37.c
+++ lab2/ex37.c
@@ -7,7 +7,7 @@ int main()
     printf("Digite o valor do produto: ");
     scanf("%f", &p);
 
-    printf("O valor desse produto com desconto de 12%% eh: %f", p*0.88);
+    printf("O valor desse produto com desconto de 12%% eh: %f reais", p*0.88);
 
     return 0;
 }
